88149e3d950e62f2fe0fa6faba1df9dfd98cd531
Alena Kuznetsova <rgb.imp@gmail.com>
2021-03-28 20:13:21 +0300

    feat: realizate functions count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let count = 0;
+  if (!!backyard.length) {
+    backyard.reduce( (a,b) => a.concat(b))
+          .forEach( item => (item === '^^') ? count++ : count);
+  }
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  if (Array.isArray(team)) {
+    let nameOfTeam = [];
+    team.forEach( item => (typeof(item) === 'string') ? nameOfTeam.push(item.replace(/^\s+/, '')[0].toUpperCase()) : nameOfTeam);
+    return nameOfTeam.sort().join('');
+  }
+  return false;
 };
