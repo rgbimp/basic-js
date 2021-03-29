010a72f331cce3c84db33a214972e708f107e68d
Alena Kuznetsova <rgb.imp@gmail.com>
2021-03-29 10:06:44 +0300

    feat: realize hanoi-tower and what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const seasons = Object.entries({
+    'winter': [11, 0, 1],
+    'spring': [2,3,4],
+    'summer': [5,6,7],
+    'autumn': [8,9,10] 
+  });
+
+  if (date === undefined) {return 'Unable to determine the time of year!';}
+  if (date instanceof Date && !date.hasOwnProperty('getMonth')){
+    let month = date.getMonth()
+    for (let [key, value] of seasons) {
+      if (value.includes(month)) {return key}
+    }
+  } else {
+  throw new Error();
+  }
 };
