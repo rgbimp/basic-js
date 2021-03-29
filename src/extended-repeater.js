2ebc2bf8cfb73e96dd9d90c8ed5268928a37be83
Alena Kuznetsova <rgb.imp@gmail.com>
2021-03-29 20:23:40 +0300

    feat: realize extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  
+  if (options) {
+    let resultStr = '';
+    let {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;
+    let additionArr = new Array(additionRepeatTimes);
+    let tempStr = `${str}${additionArr.fill(`${addition}`).join(additionSeparator)}`;
+    let strArr = new Array(repeatTimes);
+    resultStr = strArr.fill(tempStr).join(separator);
+    return resultStr;
+  }
+  return str;
 };
   
