5992f9a95040567216e376d6b11ed776f48b7582
Alena Kuznetsova <rgb.imp@gmail.com>
2021-03-29 23:57:56 +0300

    feat: realize part of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if (Array.isArray(array)) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i];
+      switch (item) {
+        case '--discard-next':
+          i++
+          break;
+        case '--discard-prev':
+          result.pop();
+          break;
+        case '--double-next':
+          if ( i + 1 < array.length) result.push(array[i + 1]);
+          break;
+        case '--double-prev':
+          if ( i - 1 >= 0) result.push(array[i - 1]);
+          break;
+        default:
+          result.push[item];
+          break;
+      }
+      
+    }
+    return result;
+  } else throw new Error();
+
 };
